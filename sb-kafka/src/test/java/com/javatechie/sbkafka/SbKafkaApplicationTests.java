d4e9db3d41822d04add613b6d7d73bd3b9adc498
honeyguy <rakanju2006@gmail.com>
2023-02-03 17:17:58 +0800

    Kafka-3.2 First commit

---
+++ sb-kafka/src/test/java/com/javatechie/sbkafka/SbKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.sbkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
